be73c5396df700b5a8db82dc8d06c1266974e130
Stefon Johns <31137952+StefJohns@users.noreply.github.com>
2018-03-28 09:44:01 -0700

    Initial Build and Configuration
    Random Facts Version 1.0.0
    --Language: Java
    --IDE: Android Studio

---
+++ RandomFacts/app/src/main/java/io/github/stefjohns/randomfacts/RandomFactActivity.java
@@ -0,0 +1,13 @@
+package io.github.stefjohns.randomfacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RandomFactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random_fact);
+    }
+}
